9aba1d7421c3e017ab7033596044da5577a4d2a4
Randy Biglow <RandyBiglow@Randys-MacBook-Pro.local>
2016-05-31 12:57:15 -0700

    Adds dummy markers.

--- app/src/main/java/com/companyname/ceramicgod/MapsActivity.java
+++ app/src/main/java/com/companyname/ceramicgod/MapsActivity.java
@@ -5,8 +5,11 @@ import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -39,8 +42,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng inglewood = new LatLng(34.01,-118.40);
+        LatLng santaMonica = new LatLng(34.02, -118.49);
+        LatLng craftsman = new LatLng(34.013235, -118.496131);
+        LatLng gA = new LatLng(34.012982, -118.495196);
+        mMap.addMarker(new MarkerOptions().position(santaMonica).title("Marker at Santa Monica"));
+        mMap.addMarker(new MarkerOptions().position(inglewood).title("Marker at Inglewood"));
+        mMap.addMarker(new MarkerOptions().position(craftsman).title("Marker at Craftsman"));
+        mMap.addMarker(new MarkerOptions().position(gA).title("Marker at GA"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(santaMonica));
+
+        //Gives the default view of the map to a closer location.
+        //Target sets the default location.
+        //Zoom of 15 allows user to see by streets. BUT it limits the view. Will come back to.
+        //Need to implement onItemClickListener to any parts of the map to allow for additional zooming options.
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(santaMonica)
+                .zoom(17)
+                .tilt(0)
+                .bearing(0)
+                .build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        MapFragment.newInstance(new GoogleMapOptions().camera(cameraPosition));
     }
+
+    //TODO Add to database.
 }
