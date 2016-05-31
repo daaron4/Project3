f90143d547afd68eafa6ee7041a0099365510bd3
changwinstonr <chang.winstonr@gmail.com>
2016-05-31 11:19:09 -0700

    Adds: DetailView. Attempted: Universal Loader URI. :sunrise:

---
+++ app/src/main/java/com/companyname/ceramicgod/DetailView.java
@@ -0,0 +1,13 @@
+package com.companyname.ceramicgod;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DetailView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_view);
+    }
+}
--- app/src/main/java/com/companyname/ceramicgod/MainActivity.java
+++ app/src/main/java/com/companyname/ceramicgod/MainActivity.java
@@ -1,13 +1,24 @@
 package com.companyname.ceramicgod;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    DetailView mDetailButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);}
+
+        public void sendDetailView(View view){
+            Intent intent = new Intent(this, DetailView.class);
+            startActivity(intent);
+
+        }
+
     }
-}
+
