e47586701a1d22006edc9d99c477ba3315b6dc83
changwinstonr <chang.winstonr@gmail.com>
2016-06-03 12:33:53 -0700

    Changes: layout.fragment. Commits for checkout error.

--- app/src/main/java/com/companyname/ceramicgod/ToiletFragment.java
+++ app/src/main/java/com/companyname/ceramicgod/ToiletFragment.java
@@ -18,7 +18,7 @@ public class ToiletFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_nearby, container, false);
+        View view = inflater.inflate(R.layout.fragment_toilet, container, false);
         mToiletView = (ImageView) view.findViewById(R.id.toilet_image_view);
         mSignView = (ImageView) view.findViewById(R.id.toilet_sign_image_view);
         Flickr.getInstance((PhotoCallback)getActivity()).doRequest();
